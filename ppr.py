c4561cf8dc199d81313f14a23c6e6bbcd0aa7410
nimishabhide <55435515+nimishabhide@users.noreply.github.com>
2020-10-29 16:19:17 +0530

    Update ppr.py

--- ppr.py
+++ ppr.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-
+from gtts import gTTS
+import os
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 html_temp = """
@@ -75,19 +76,29 @@ if st.button("Regression"):
     r4=r2_score(y4_test, y4_pred)
     print(r4)
     if(((r1>r2) and (r1>r3)) and (r1>r4)):
+        p="Please use multiple regression for best results"
         st.write("Multiple regression")
         st.write(r1)
     elif(((r2>r1) and (r2>r3)) and (r2>r4)):
+        p="Please use plolynomial regression for best results"
         st.write("Polynomial regression")
         st.write(r2)
     elif(((r3>r2) and (r3>r1)) and (r3>r4)):
+        p="Please use decision tree regression for best results"
         st.write("Decision Tree regression")
         st.write(r3)
     elif(((r4>r2) and (r4>r3)) and (r4>r1)):
+        p="Please use random forest regression for best results"
         st.write("Random Forest regression")
         st.write(r4)
     else:
+        p="Please use XGBoost as all other regression techniques are not good enough and XGBoost yeilds the best results for your data"
         st.write('Use XGBoost for regression')
+    output=gTTS(text=p,lang=language,slow=False)
+    output.save("voice.ogg")
+    audio_file = open('voice.ogg', 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format='audio/ogg')
 if st.button("Classification"):
     d=pd.read_csv(uploaded_file)
     d1=d.copy()
@@ -200,28 +211,40 @@ if st.button("Classification"):
     from sklearn.metrics import accuracy_score
     acc7=accuracy_score(b7_test, b7_pred)
     if(((acc1>acc2) and (acc1>acc3)) and ((acc1>acc4) and (acc1>acc5)) and ((acc1>acc6) and (acc1>acc7))):
+        q="Please use logistic regression for best results"
         st.write('Logistic Regression')
         st.write(acc1)
     elif(((acc2>acc1) and (acc2>acc3)) and ((acc2>acc4) and (acc2>acc5)) and ((acc2>acc6) and (acc2>acc7))):
+        q="Please use KNN classification for best results"
         st.write('KNN')
         st.write(acc2)
     elif(((acc3>acc2) and (acc3>acc1)) and ((acc3>acc4) and (acc3>acc5)) and ((acc3>acc6) and (acc3>acc7)) ):
+        q="Please use SVM(Linear) classification for best results"
         st.write('SVM(Linear)')
         st.write(acc3)
     elif(((acc4>acc2) and (acc4>acc3)) and ((acc4>acc1) and (acc4>acc5)) and ((acc4>acc6) and(acc4>acc7)) ):
+        q="Please use SVM(Kernel) classification for best results"
         st.write('SVM(Kernel)')
         st.write(acc4)
     elif(((acc5>acc2) and (acc5>acc3)) and ((acc5>acc4) and (acc5>acc1)) and ((acc5>acc6) and (acc5>acc7)) ):
+        q="Please use Naive Bayes classification for best results"
         st.write('Naive Bayes')
         st.write(acc5)
     elif(((acc6>acc2) and (acc6>acc3)) and ((acc6>acc4) and (acc6>acc5)) and ((acc6>acc1) and (acc6>acc7)) ):
+        q="Please use Decision Tree classification for best results"
         st.write('Decision Tree')
         st.write(acc6)
     elif(((acc7>acc2) and (acc7>acc3)) and ((acc7>acc4) and (acc7>acc5)) and ((acc7>acc1) and (acc7>acc7)) ):
+        q="Please use Random Forest classification for best results"
         st.write('Random Forest')
         st.write(acc6)
     else:
+        q="Please use XGBoost as all other classification techniques are not good enough and XGBoost yeilds the best results for your data"
         st.write('Use XGBoost for Classification')
+    output=gTTS(text=q,lang=language,slow=False)
+    output.save("voice.ogg")
+    audio_file = open('voice.ogg', 'rb')
+    audio_bytes = audio_file.read()
 
 
 
